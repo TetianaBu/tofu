9b3ff3d5157bb6496b52b62bcb3afaa1cc0b1974
tetianataker <tetianataker@gmail.com>
2023-10-29 18:49:52 +0100

    hooks useToogle, useEscapeKey

---
+++ app/lib/hooks/use-escape-key.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+function useEscapeKey(callback) {
+  useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === "Escape") {
+        callback();
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [callback]);
+}
+
+export default useEscapeKey;
