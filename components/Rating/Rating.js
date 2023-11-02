2808b01fe7658250b69a4aa78ac73254a0a01990
tetianataker <tetianataker@gmail.com>
2023-11-02 19:34:02 +0100

    Rating

---
+++ components/Rating/Rating.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Star } from "react-feather";
+import styles from "./Rating.module.css";
+import { range } from "../../lib/range";
+
+function Rating({ rating, maxRating = 5 }) {
+  return (
+    <div>
+      <div className={styles.stars}>
+        {range(rating).map((num) => (
+          <Star key={num} />
+        ))}
+      </div>
+      <div>
+        {" "}
+        Rating: <span className={styles.number}>{rating}</span> / {maxRating}
+      </div>
+    </div>
+  );
+}
+export default Rating;
